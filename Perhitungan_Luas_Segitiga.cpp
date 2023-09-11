1c4817c648c7f26eead7141a6d961ad32da38664
Elderrss <144640867+Elderrss@users.noreply.github.com>
2023-09-11 09:25:14 +0700

    Create Perhitungan_Luas_Segitiga.cpp

---
+++ Perhitungan_Luas_Segitiga.cpp
@@ -0,0 +1,14 @@
+#include<iostream> 
+using namespace  std;
+
+int main ( ){
+      int a, t ;
+      cout<<"MENGHITUNG LUAS SEGITIGA \n" ;
+      cout<<"############################### \n";
+      cout<<"Masukan Alas: ";
+      cin>>a ;
+      cout<< "Masukan Tinggi: ";
+      cin>>t ;
+      cout<<"Hasil Luas Segitiga Adalah: "<<0.5*a*t<<endl;
+      return 0;
+}
